2dfed282e0a2edf891a2854a67963875e62cfe52
이채윤 <161849407+cchaeyoon@users.noreply.github.com>
2026-06-13 18:57:07 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,17 +1,11 @@
 def solution(id_pw, db):
-    answer = ''
-    wrongpw = 0
+    answer = 'fail'
     for i in db:
-        if i[0] == id_pw[0] and i[1] == id_pw[1]:
-            answer = 'login'
-            continue
-        elif i[0] == id_pw[0] and i[1] != id_pw[1]:
-            wrongpw += 1
-            continue
+        if i[0] == id_pw[0]:
+            if i[1] == id_pw[1]:
+                answer = 'login'
+            else:
+                answer = 'wrong pw'
         else:
-            continue
-    if wrongpw == 1:
-        answer = 'wrong pw'
-    elif wrongpw == 0 and answer != 'login':
-        answer = 'fail'
+             continue 
     return answer
